a5d5ee3852a22fd6f1df7b6dddbd2f402d82fd6c
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-02-14 13:38:37 -0700

    hehe

---
+++ Mission_06Easton/Models/Movies.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+namespace Mission_06Easton.Models
+{
+    public class Movies
+    {
+        [Key]
+        [Required]
+        //all the database stuff
+        public int MovieID { get; set;}
+        //add the rest
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+
+        public string Rating { get; set; }
+
+        public Boolean Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+
+}
+
+
+//this is the database model set up
+//
